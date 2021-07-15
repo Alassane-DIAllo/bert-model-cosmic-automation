64d62e6e24874636b3c357612f3faf7b537f56a6
Alassane Diallo <dialloalassann@yahoo.fr>
2021-07-15 14:02:30 +0200

    deployment on local machine

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, jsonify, request 
-from bert_model import *
-import os
-app = Flask(__name__)
+import bert_model as model
+import os 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
+
+app = Flask(__name__)
 @app.route('/')
 def hello():    
     return 'Alright , Server runs fine !'
 
-@app.route('/get_sentiment', method=['POST'])
-def get_sentiment():
+@app.route('/get_NER', methods=['POST'])
+def get_NER():
     x = request.get_json(force=True)
-    NameEntity = model.get_predictor(x)
-    
-    return jsonify(result=sent)
-
+    text = x["sentence"]
+    NameEntity = model.get_predictor(text)
+    return  jsonify(result = NameEntity)
     
 if __name__=='__main__':
     app.run(host='localhost', port=5000, debug=True, use_reloader=False)
